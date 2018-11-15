d0350156b392830d2c4d1bb9cd81ea3d19860a8d
Denys Lins <denyslins@gmail.com>
2018-11-15 14:15:59 -0300

    Jumping on the Clouds solution

---
+++ Jumping on the Clouds/solution.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c) {
+    const arrayLength = c.length;
+    let numSteps = 0;
+    for (let i = 0; i < arrayLength; i++) {
+        if (i + 2 < arrayLength && c[i + 2] != 1) {
+            numSteps += 1;
+            i += 1;
+        } else {
+            numSteps += 1;
+        }
+    }
+    return numSteps - 1;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = jumpingOnClouds(c);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
